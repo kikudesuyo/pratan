8115418b0323af484f4293a4b9215099280c877b
kikudesuyo <kikuchi.hiromu.609@s.kyushu-u.ac.jp>
2024-07-18 00:05:16 +0900

    create new page

---
+++ frontend/src/pages/WordDetail.tsx
@@ -0,0 +1,65 @@
+import { useState } from "react";
+
+type WordCardProps = {
+  word: string;
+};
+
+const WordDetail = ({ word }: WordCardProps) => {
+  const [activeMode, setActiveMode] = useState("explanation");
+
+  const modes = [
+    { id: "explanation", label: "Explanation" },
+    { id: "related", label: "Related" },
+    { id: "derivative", label: "Derivative" },
+  ];
+
+  const getContent = () => {
+    switch (activeMode) {
+      case "explanation":
+        return "(of language) fluent or persuasive; (of a person) having the ability to speak or write persuasively";
+      case "related":
+        return "articulate (明確な), expressive (表現力のある), fluent (流暢な)";
+      case "derivative":
+        return "eloquently (adv. 雄弁に), eloquence (n. 雄弁), eloquentness (n. 雄弁)";
+      default:
+        return "";
+    }
+  };
+
+  return (
+    <div className="bg-white rounded-lg shadow-lg p-6">
+      <h1 className="text-4xl font-bold mb-6 text-center text-custom-primary">
+        {word}
+      </h1>
+      <div className="flex justify-center mb-6">
+        <div className="inline-flex rounded-md shadow-sm" role="group">
+          {modes.map(({ id, label }) => (
+            <button
+              key={id}
+              onClick={() => setActiveMode(id)}
+              className={`
+                px-4 py-2 text-lg font-medium border border-custom-primary
+                ${
+                  activeMode === id
+                    ? "bg-orange-400 text-white"
+                    : "bg-white text-custom-primary"
+                }
+                ${id === "explanation" ? "rounded-l-lg" : ""}
+                ${id === "derivative" ? "rounded-r-lg" : ""}
+                hover:bg-orange-400 hover:text-white focus:z-10 focus:ring-2
+                focus:ring-orange-400 focus:text-white transition duration-300
+              `}
+            >
+              {label}
+            </button>
+          ))}
+        </div>
+      </div>
+      <div className="min-h-[200px] p-4 border border-gray-200 rounded bg-gray-50 text-lg">
+        {getContent()}
+      </div>
+    </div>
+  );
+};
+
+export default WordDetail;
--- frontend/src/utils/constants/Paths.ts
+++ frontend/src/utils/constants/Paths.ts
@@ -1,4 +1,5 @@
 export const PATHS = {
-  WORDS: "/words",
+  WORDS: "/word-detail",
+  WORDLIST: "word-list",
   PRODUCT: "/product",
 } as const;
