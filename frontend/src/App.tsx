bc854219665d4f0e67144133f33d7536e9d7ac5d
kikudesuyo <kikuchi.hiromu.609@s.kyushu-u.ac.jp>
2024-07-18 00:05:35 +0900

    display

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,18 +1,24 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "@/assets/styles/destyle.css";
 
-import Words from "@/pages/Words";
+import WordCards from "@/pages/WordCards";
+import WordDetail from "@/pages/WordDetail";
+import ScrollToTop from "@/utils/ScrollTop";
 import Header from "@/components/Header";
+import Footer from "@/components/Footer";
 import { PATHS } from "@/utils/constants/Paths";
 
 function App() {
   return (
     <div className="flex min-h-screen flex-col">
       <Router>
+        <ScrollToTop />
         <Header />
         <Routes>
-          <Route path={PATHS.WORDS} element={<Words />} />
+          <Route path={PATHS.WORDS} element={<WordDetail word="eloquent" />} />
+          <Route path={PATHS.WORDLIST} element={<WordCards />} />
         </Routes>
+        <Footer />
       </Router>
     </div>
   );
