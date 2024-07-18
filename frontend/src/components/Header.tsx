0d838d407455015d1fc05c34fe5ef618685b493c
kikudesuyo <kikuchi.hiromu.609@s.kyushu-u.ac.jp>
2024-07-18 09:07:32 +0900

    use button.tsx

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,16 +1,14 @@
+import Button from "@/components/Button";
+
 const Header = () => {
   return (
     <header className="sticky top-0 flex h-16 justify-between border-b-2 border-slate-300 bg-white p-3 shadow-md">
       <div className="flex flex-col justify-center text-xl font-bold">
-        <p>Pratan📙</p>
+        <button>Pratan📙</button>
       </div>
-      <div className="flex gap-2">
-        <p className="rounded-2xl bg-slate-300 px-4 py-2 text-center font-bold hover:bg-gray-400">
-          Login
-        </p>
-        <p className="rounded-2xl bg-slate-300 px-4 py-2 text-center font-bold hover:bg-gray-400">
-          Signup
-        </p>
+      <div className="flex gap-4">
+        <Button label="Login" func={() => {}} />
+        <Button label="Signup" func={() => {}} />
       </div>
     </header>
   );
