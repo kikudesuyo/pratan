c88ca3a2934fa8ec080ed99d664b229dd385f693
kikudesuyo <kikuchi.hiromu.609@s.kyushu-u.ac.jp>
2024-07-19 22:10:37 +0900

    ボタン関連を修正

--- frontend/src/components/Button/AuthButton.tsx
+++ frontend/src/components/Button/AuthButton.tsx
@@ -1,10 +1,14 @@
-import { Props } from "@/components/Button/types";
+type Props = {
+  label: string;
+  func: () => void;
+};
 
-const AuthButton = ({ label, style = "", func }: Props) => {
-  //styleでボタンの色を設定(hover:も設定)
+const AuthButton = ({ label, func }: Props) => {
   return (
     <button
-      className={`rounded-2xl text-center font-bold  ${style}`}
+      className={
+        "rounded-2xl text-center font-bold bg-slate-300 hover:bg-gray-400 px-4"
+      }
       onClick={func}
     >
       {label}
---
+++ frontend/src/components/Button/BodyButton.tsx
@@ -0,0 +1,17 @@
+type Props = {
+  label: string;
+  func: () => void;
+};
+
+const BodyButton = ({ label, func }: Props) => {
+  return (
+    <button
+      className="bg-orange-400 hover:bg-orange-500 py-4 px-8 text-xl rounded-2xl text-center font-bold"
+      onClick={func}
+    >
+      {label}
+    </button>
+  );
+};
+
+export default BodyButton;
