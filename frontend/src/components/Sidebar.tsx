1e232e6a283674e98e2ead0d788431b0635b4da6
kikudesuyo <kikuchi.hiromu.609@s.kyushu-u.ac.jp>
2024-07-20 15:16:12 +0900

    スタイルの調整

--- frontend/src/components/Sidebar.tsx
+++ frontend/src/components/Sidebar.tsx
@@ -1,68 +1,99 @@
-import { useState, useRef, useEffect } from "react";
-import { Link } from "react-router-dom";
+import React from "react";
+import { slide as Menu } from "react-burger-menu";
+import { NavLink, useLocation } from "react-router-dom";
 import SidebarButton from "@/components/Button/SidebarButton";
-import { Transition } from "@headlessui/react";
 import { PATHS } from "@/utils/constants/Paths";
-import LoginLogo from "@/assets/logo/LoginLogo";
+import TestLogo from "@/assets/logo/TestLogo";
+import SignOutLogo from "@/assets/logo/Signout";
+import HomeLogo from "@/assets/logo/Home";
 
 const Sidebar: React.FC = () => {
-  const [isOpen, setIsOpen] = useState(false);
-  const sidebarRef = useRef<HTMLDivElement>(null);
-  const toggleSidebar = () => setIsOpen(!isOpen);
-  console.log(isOpen);
+  const [isOpen, setIsOpen] = React.useState(false);
+  const location = useLocation();
 
-  useEffect(() => {
-    const handleClickOutside = (event: MouseEvent) => {
-      if (
-        sidebarRef.current &&
-        !sidebarRef.current.contains(event.target as Node)
-      ) {
-        setIsOpen(false);
-      }
-    };
+  React.useEffect(() => {
+    setIsOpen(false);
+  }, [location]);
 
-    if (isOpen) {
-      document.addEventListener("mousedown", handleClickOutside);
-    } else {
-      document.removeEventListener("mousedown", handleClickOutside);
-    }
+  const handleStateChange = (state: { isOpen: boolean }) => {
+    setIsOpen(state.isOpen);
+  };
 
-    return () => {
-      document.removeEventListener("mousedown", handleClickOutside);
-    };
-  }, [isOpen]);
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const closeMenu = () => {
+    setIsOpen(false);
+  };
+  const styles = {
+    bmMenuWrap: {
+      position: "fixed",
+      height: "100%",
+    },
+    bmMenu: {
+      background: "#ffaf7d",
+      padding: "2.5em 1.5em 0",
+      fontSize: "1.15em",
+    },
+    bmItemList: {
+      color: "#b8b7ad",
+      padding: "0.8em",
+      height: "100%",
+    },
+    bmOverlay: {
+      background: "rgba(0, 0, 0, 0)",
+    },
+  };
 
   return (
     <>
-      <SidebarButton func={toggleSidebar} />
-      <Transition
-        show={isOpen}
-        enter="transition-transform duration-300 ease-out"
-        enterFrom="transform translate-x-full"
-        enterTo="transform translate-x-0"
-        leave="transition-transform duration-300 ease-in"
-        leaveFrom="transform translate-x-0"
-        leaveTo="transform translate-x-full"
+      <Menu
+        isOpen={isOpen}
+        onStateChange={handleStateChange}
+        right
+        styles={styles}
+        outerContainerId={"outer-container"}
+        pageWrapId={"page-wrap"}
+        customBurgerIcon={false}
+        customCrossIcon={false}
       >
-        <div
-          ref={sidebarRef}
-          className="fixed right-0 top-0 h-full w-72 bg-gray-100 bg-gradient-to-b p-6 shadow-xl"
-        >
-          <nav className="flex h-full flex-col justify-between">
-            <ul className="space-y-6">
-              <li>
-                <Link
-                  to={PATHS.WORDLIST}
-                  className="flex items-center transition-colors duration-200"
-                >
-                  <LoginLogo />
-                  <span className="text-lg font-semibold">Home</span>
-                </Link>
-              </li>
-            </ul>
-          </nav>
+        <div className="flex h-full flex-col justify-between">
+          <div className="flex flex-col gap-8">
+            <NavLink
+              className="block border-b py-2 text-white hover:text-gray-300"
+              to="/"
+              onClick={closeMenu}
+            >
+              <div className="flex items-center">
+                <HomeLogo />
+                <span className="">Home</span>
+              </div>
+            </NavLink>
+            <NavLink
+              className="block border-b py-2 text-white hover:text-gray-300"
+              to={PATHS.WORDDETAIL}
+              onClick={closeMenu}
+            >
+              <div className="flex items-center">
+                <TestLogo />
+                <span>Test</span>
+              </div>
+            </NavLink>
+            <NavLink
+              className="block flex-1 border-b py-2 text-white hover:text-gray-300"
+              to={PATHS.WORDDETAIL}
+              onClick={closeMenu}
+            >
+              <div className="flex items-center">
+                <SignOutLogo />
+                <span>Signout</span>
+              </div>
+            </NavLink>
+          </div>
         </div>
-      </Transition>
+      </Menu>
+      <SidebarButton func={toggleMenu} />
     </>
   );
 };
