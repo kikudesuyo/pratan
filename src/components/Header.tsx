1abb3e81bc708c4e0b887940f9e177af16ea0f83
Ikuya Nagaki <ikuya313198@gmail.com>
2024-07-21 07:49:30 +0900

    fix: hide sidebar when user is logged in

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,10 +4,11 @@ import { Link, useNavigate } from "react-router-dom";
 import AuthButton from "@/components/Button/AuthButton";
 import Sidebar from "@/components/Sidebar";
 import { PATHS } from "@/utils/constants/Paths";
+import useUserStore from "@/stores/user";
 
 const Header = () => {
   const navigate = useNavigate();
-  const user = true;
+  const userId = useUserStore((state) => state.userId);
 
   return (
     <header className="sticky top-0 z-10 flex h-16 justify-between border-b-2 border-slate-300 bg-white p-3 shadow-md">
@@ -15,7 +16,7 @@ const Header = () => {
         <Link to={PATHS.LANDING}>Pratan📙</Link>
       </div>
       <div className="flex gap-4">
-        {!user ? (
+        {!userId ? (
           <>
             <AuthButton
               label="Login"
