61c69f64ef21c1f19c9e56752e4518a8b0dbdab4
kikudesuyo <kikuchi.hiromu.609@s.kyushu-u.ac.jp>
2024-07-29 11:42:20 +0900

    変数名を修正(newWord→spell)

--- src/features/addWord.ts
+++ src/features/addWord.ts
@@ -3,19 +3,17 @@ import { useWordsStore } from "@/stores/words";
 import { registerWord } from "@/features/registerWord";
 import { addWord as addWordWithZustand } from "@/stores/words";
 
-export const addWord = async (newWord: string) => {
+export const addWord = async (spell: string) => {
   const userId = useUserStore.getState().userId;
   const words = useWordsStore.getState().words;
   if (!userId) {
     console.error("User is not logged in");
     throw new Error("User is not logged in");
   }
-  if (
-    words.some((word) => word.spell.toLowerCase() === newWord.toLowerCase())
-  ) {
+  if (words.some((word) => word.spell.toLowerCase() === spell.toLowerCase())) {
     alert("This word is already in the list");
     throw new Error("This word is already in the list");
   }
-  const word = await registerWord(userId, newWord);
+  const word = await registerWord(userId, spell);
   addWordWithZustand(word);
 };
