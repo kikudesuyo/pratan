7ed2d0cf3a583f84f754d03c609dbe566f0f7406
kikudesuyo <kikuchi.hiromu.609@s.kyushu-u.ac.jp>
2024-07-29 11:12:09 +0900

    単語登録のリファクタリング

--- src/pages/WordList.tsx
+++ src/pages/WordList.tsx
@@ -1,33 +1,18 @@
-import { registerWord } from "@/features/registerWord";
 import { useNavigate } from "react-router-dom";
 import { useState } from "react";
-import useUserStore from "@/stores/user";
 import WordCard from "@/pages/WordCard";
-import { useWordsStore, addWord } from "@/stores/words";
+import { useWordsStore } from "@/stores/words";
 import { PATHS } from "@/utils/constants/Paths";
+import { addWord } from "@/features/addWord";
 
 const WordList = () => {
   const [newWordSpell, setNewWordSpell] = useState("");
   const [searchTerm, setSearchTerm] = useState("");
-  const userId = useUserStore((state) => state.userId);
   const words = useWordsStore((state) => state.words);
   const navigate = useNavigate();
 
   const handleAddClick = async () => {
-    if (!userId) {
-      console.error("User is not logged in");
-      return;
-    }
-    if (
-      words.some(
-        (word) => word.spell.toLowerCase() === newWordSpell.toLowerCase()
-      )
-    ) {
-      alert("This word is already in the list");
-      throw new Error("This word is already in the list");
-    }
-    const word = await registerWord(userId, newWordSpell);
-    addWord(word);
+    await addWord(newWordSpell);
     setNewWordSpell("");
   };
 
